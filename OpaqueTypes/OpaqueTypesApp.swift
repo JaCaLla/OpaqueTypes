dfde38298843edb72a7065d14033a9710d6c364f
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-05 20:17:29 +0100

    Initial Commit

---
+++ OpaqueTypes/OpaqueTypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpaqueTypesApp.swift
+//  OpaqueTypes
+//
+//  Created by Javier Calatrava on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct OpaqueTypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
